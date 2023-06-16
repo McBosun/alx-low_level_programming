07b625e2a89f7d45baa497b4f48542c8100b24d6
McBosun <olatubosunabi4@gmail.com>
2023-06-15 17:54:42 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main A line with puts functions
+ * return: 0 (successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
