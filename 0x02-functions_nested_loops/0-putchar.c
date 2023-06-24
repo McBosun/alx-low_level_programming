09ac2b1746c3e50784071972a4620ea0ca2917f5
McBosun <olatubosunabi4@gmail.com>
2023-06-24 09:19:51 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include <main.h>
+
+/**
+ * main - about to enter my code with a unique input
+ *
+ * Return: always
+ */
+
+int main(void)
+{
+	_putchar('w');
+	_putchar('r');
+	_putchar('i');
+	_putchar('t');
+	_putchar('e');
+	_putchar('a');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
